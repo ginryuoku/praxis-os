74994400d3e0b048b55942a5f68d43a6c5a8dfbc
Michelle Darcy <mdarcy137@gmail.com>
2019-06-18 11:42:47 -0700

    Fake test runner (suppresses RLS error)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 extern crate panic_halt;
 
@@ -7,5 +9,11 @@ use riscv_rt::entry;
 
 #[entry]
 fn main() -> ! {
+    loop { }
+}
+
+#[cfg(test)]
+fn test_runner(_tests: &[&dyn Fn()]) {
+    //println!("Running {} tests", tests.len());
     loop { }
 }
